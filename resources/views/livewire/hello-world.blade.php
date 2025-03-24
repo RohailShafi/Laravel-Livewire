6b16bda701d56fc01804ba77857b342731992d6b
Rohail Shafi <115107849+aamir79@users.noreply.github.com>
2025-03-24 14:09:06 +0500

    Livewire Installation

---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,9 @@
+<div class="container py-5">
+    {{-- Do your work, then step back. --}}
+    The current time is : {{ time() }}
+{{--    <br>--}}
+    <button class="btn btn-primary" wire:click="$refresh">Refresh</button>
+
+    <h1>hello Internet</h1>
+    <p>Internet is very interesting thing I love to use it for browsing and for social media</p>
+</div>
